7789cdd0ad726c657cec5143b26095c125d176ab
tuantiphu1 <tuantiphu1@gmail.com>
2026-06-26 15:43:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Xin chào! XSMB Bot đang chạy.")
+import os
+import requests
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+data = {
+    "chat_id": CHAT_ID,
+    "text": "🎉 Xin chào! Bot đã kết nối thành công với GitHub Actions."
+}
+
+response = requests.post(url, data=data)
+
+print(response.status_code)
+print(response.text)
