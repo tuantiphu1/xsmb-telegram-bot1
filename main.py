8347b1ab9fe2568809c2d98370bc4ee613710910
tuantiphu1 <tuantiphu1@gmail.com>
2026-06-27 19:24:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from openai import OpenAI
 
 client = OpenAI(
-    api_key=os.environ["OPENAI_API_KEY"]
+    api_key=os.environ["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"]
 )
 
 response = client.chat.completions.create(
